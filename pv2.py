c8d41b188664de910ea1e02cd82d27fd92c21105
ProPote <97349764+ProPote@users.noreply.github.com>
2023-07-03 12:49:33 +0530

    Update pv2.py

--- pv2.py
+++ pv2.py
@@ -1,22 +1,7 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      User
-#
-# Created:     30/06/2023
-# Copyright:   (c) User 2023
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 from tkinter import *
 import time
 from inputimeout import inputimeout
-cloudcover=""
-C1="df"
-C2=""
-C3=""
-C4=""
+
 #def frontend_initial():
 #FINAL_METAR="ABCD"
 def backend():
@@ -47,42 +32,50 @@ def backend():
                 e4.pack()
                 e5=Entry(root)
                 e5.pack()
-                C1=e2.get()
-                C2=e3.get()
-                C3=e4.get()
-                C4=e5.get()
-                cloudcover=C1+C2+C3+C4
+                #C1=e2.get()
+                #C2=e3.get()
+                #C3=e4.get()
+                #C4=e5.get()
+                def cloud_cover():
+                    cloudcover=e2.get()+" "+e3.get()+" "+e4.get()+" "+e5.get()
+                    print(cloudcover)
+                    return cloudcover
                 def onclic():
-                    time.sleep(10)
+                    time.sleep(0)
                     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-                    runway_number=data[10:12]
-                    GST_Time=data[22:30]
-                    Date=data[31:42]
-                    WS_10_min=data[63:66]
-                    WD_10_min=data[86:88]
-                    Hum_10_min=data[104:106]
-                    Temp_10_min=data[131:136]
-                    DP_10_min=data[155:160]
-                    Pres_10_min=data[183:188]
-                    QNH_10_min=data[217:223]
-                    QFE_10_min=data[246:251]
+                    data2=data.decode('utf-8')
+                    print(data2)
+                    runway_number=data2[8:10]
+                    GST_Time=data2[20:28]
+                    Date=data2[29:31]
+                    WS_10_min=data2[61:64]
+                    WD_10_min=data2[86:88]
+                    Hum_10_min=data2[107:109]
+                    Temp_10_min=data2[130:132]
+                    DP_10_min=data2[154:156]
+                    Pres_10_min=data2[185:190]
+                    QNH_10_min=data2[215:219]
+                    QFE_10_min=data2[237:240]
+
+                    print(GST_Time)
+                    print(Date)
                     #declaring all the components of the final METAR Report
                     METAR=""
                     report=" "
                     typ = "METAR"
-                    #-----------------------------------------------------------------------
+                    #-----------------------------------------------------------
                     #Taking the timestamp for which the user wants the METAR
                     #Finding the row in the given data which has to be accessed
                     #row=0
                     #for i in range(25):
                     #    if time==df.iloc[i,3]:
                     #        row=i
-                    #---------------------------------------------------------------------------
+                    #-----------------------------------------------------------
 
                     #Taking the four terms of cloud cover
                     #cell_value = df.iloc[row, 28] # cell_value = df.iloc[row_index, column_index]
                     #pressure=('Q -',cell_value)
-                    #---------------------------------------------------------------------------
+                    #-----------------------------------------------------------
 
                     #status term which comprises of date,and time in zulu
                     time1=""
@@ -90,24 +83,24 @@ def backend():
                     def status():
                         #runway=(df.iloc[row,2])
                         time_used=GST_Time
-                        if time_used[3]==':':time1='0'+time_used[2]+time_used[4]+time_used[5]
-                        else:time1=time_used[2]+time_used[3]+time_used[5]+time_used[6]
+                        time1=time_used[0]+time_used[1]+time_used[3]+time_used[4]
                         check=Date
-                        if check[1]=='-':date='0'+check[0]
-                        else: date=check[0]+check[1]
+
                         #print(date)
-                        print(date+time1)
-                        return date+time1
-                        #---------------------------------------------------------------------------
+                        print(Date+time1+"DDDD")
+                        return Date+time1
+                        #-------------------------------------------------------
                     #temp and dew point temp component
                     temp =Temp_10_min
                     dew_pt = DP_10_min
+                    print(Temp_10_min)
+                    print(DP_10_min)
                     finaltempdew = str(temp) + '/' + str(dew_pt)
                     print(finaltempdew)
-                    report=str(report)+str(status())+'Z'
-                    FINAL_METAR=str(typ)+report+str(cloudcover)+" "+finaltempdew
+                    report=str(status())+'Z'
+                    FINAL_METAR=str(typ)+" "+str(report)+" "+str(cloud_cover())+" "+str(finaltempdew)+" "+"Q"+str(QNH_10_min)+" "+"NOSIG="
                     print(FINAL_METAR)
-                    myLabel=Label(root,text=FINAL_METAR+""+cloudcover)
+                    myLabel=Label(root,text=FINAL_METAR)
                     myLabel.pack()
             #root=Tk()
             #myLabel=Label(root,text=FINAL_METAR)
@@ -122,7 +115,7 @@ def backend():
 
 
                 root.mainloop()
-        time.sleep(10)
+        time.sleep(0)
         frontend()
 backend()
 
