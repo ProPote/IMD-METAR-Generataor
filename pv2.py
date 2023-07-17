ff846e8d22a6f6e139f451473cf60f378bd96e10
shashwat1807 <137035394+shashwat1807@users.noreply.github.com>
2023-07-17 14:45:16 +0530

    Update pv2.py

--- pv2.py
+++ pv2.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import ttk
 from tkinter import *
 import time
 from inputimeout import inputimeout
@@ -9,15 +11,12 @@ from openpyxl import Workbook,load_workbook
 from datetime import datetime
 import pytz
 
-#def frontend_initial():
-#FINAL_METAR="ABCD"
-
 def backend():
     import socket
     UDP_IP = "192.168.103.93"
     UDP_PORT = 5000
     sock = socket.socket(socket.AF_INET, # Internet
-    socket.SOCK_DGRAM) # UDP
+    socket.SOCK_DGRAM) 
     sock.bind((UDP_IP, UDP_PORT))
 
     wb = Workbook()
@@ -27,17 +26,14 @@ def backend():
 
         def frontend():
             while True:
-
-                data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-                if data.decode('utf-8')[0]=="-": #to take the right string
+                data, addr = sock.recvfrom(1024)
+                if data.decode('utf-8')[0]=="-":
                     break
+
                 FINAL_METAR=""
-                root=Tk()
-                #function to proceed after enterinf the timestamp
+
+                root = tk.Tk()
                 cloudcover=""
-
-
-                # Function to close the window after the specified duration
                 def end_window():
                     time.sleep(0)
                     data2=data.decode('utf-8')
@@ -47,57 +43,29 @@ def backend():
                     Date=data2[29:31]
                     WS_10_min=data2[61:64]
                     WD_10_min=data2[86:88]
-                    print(WD_10_min)
                     Hum_10_min=data2[107:109]
                     Temp_10_min=data2[130:132]
                     DP_10_min=data2[154:156]
                     Pres_10_min=data2[185:190]
                     QNH_10_min=data2[215:219]
                     QFE_10_min=data2[237:240]
+                    temp =Temp_10_min
+                    dew_pt = DP_10_min
+                    finaltempdew = str(temp) + '/' + str(dew_pt)
 
-                    print(GST_Time)
-                    print(Date)
-                    #declaring all the components of the final METAR Report
                     METAR=""
                     report=" "
                     typ = "METAR"
-                    #-----------------------------------------------------------
-                    #Taking the timestamp for which the user wants the METAR
-                    #Finding the row in the given data which has to be accessed
-                    #row=0
-                    #for i in range(25):
-                    #    if time==df.iloc[i,3]:
-                    #        row=i
-                    #-----------------------------------------------------------
-
-                    #Taking the four terms of cloud cover
-                    #cell_value = df.iloc[row, 28] # cell_value = df.iloc[row_index, column_index]
-                    #pressure=('Q -',cell_value)
-                    #-----------------------------------------------------------
-
-                    #status term which comprises of date,and time in zulu
                     time1=""
                     date=""
+
                     def status():
-                        #runway=(df.iloc[row,2])
                         time_used=GST_Time
                         time1=time_used[0]+time_used[1]+time_used[3]+time_used[4]
                         check=Date
-
-                        #print(date)
-                        print(Date+time1+"DDDD")
                         return Date+time1
-                        #-------------------------------------------------------
-                    #temp and dew point temp component
-                    temp =Temp_10_min
-                    dew_pt = DP_10_min
-                    print(Temp_10_min)
-                    print(DP_10_min)
-                    finaltempdew = str(temp) + '/' + str(dew_pt)
-                    print(finaltempdew)
-                    report=str(status())+'Z'
-                    FINAL_METAR=str(typ)+" "+str(report)+" "+str(finaltempdew)+" "+"Q"+str(QNH_10_min)+" "+"NOSIG="
 
+                    FINAL_METAR=str(typ)+" "+str(report)+" "+str(finaltempdew)+" "+"Q"+str(QNH_10_min)+" "+"NOSIG="
 
                     gmt = pytz.timezone('GMT')
                     now = datetime.now(gmt)
@@ -105,95 +73,75 @@ def backend():
                     current_time = now.strftime("%H:%M:%S")
 
                     ws.append([current_time,FINAL_METAR])
-                    wb.save("IMD_Data.xlsx")
+                    wb.save("demo.xlsx")
 
-                    myLabel=Label(root,text="Idle METAR"+FINAL_METAR,font="Times 20 bold")
+                    myLabel=Label(root,text="Idle"+FINAL_METAR,font="Tektur 25")
                     myLabel.pack()
 
                     def execute():
                         root.destroy()
-                        time.sleep(0)                                                                                 #to make the window reappear again
+                        time.sleep(0)
 
-                    root.after(2000,execute)
+                    root.after(4000,execute)
                     root.mainloop()
 
-                frame=LabelFrame(root,bg="grey")
-                frame.pack()
 
-                output=Label(frame,text="Enter the four cloud terms",font="Verdana 15 ",bg="#9ad7fc")
-                output.pack()
+                root.title("METAR Generator")
+                # Simply set the theme
+                root.tk.call("source", "C:\\Users\\hp\\Azure\\azure.tcl")
+                root.tk.call("set_theme", "dark")
 
-                output=Label(frame,text="")
-                output.pack()
+                output=Label(root,text="Enter the four cloud terms",font="Tektur 20")
+                output.pack(pady = 20)
 
-                e2=Entry(frame,width=40,font=('Georgia 30'))
-                e2.pack()
+                e2=Entry(root, width = 40,borderwidth = 8,font=('Tektur 25'))
+                e2.pack(padx = 20, pady = 10)
 
-                output=Label(frame,text="")
-                output.pack()
+                       
 
-                e3=Entry(frame,width=40,font=('Georgia 30'))
-                e3.pack()
+                e3=Entry(root,width=40,borderwidth = 8,font=('Tektur 25'))
+                e3.pack(padx = 20, pady = 10)
 
-                output=Label(frame,text="")
-                output.pack()
+                        
 
-                e4=Entry(frame,width=40,font=('Georgia 30'))
-                e4.pack()
+                e4=Entry(root,width=40,borderwidth = 8,font=('Tektur 25'))
+                e4.pack(padx = 20, pady = 10)
 
-                output=Label(frame,text="")
-                output.pack()
+                        
 
-                e5=Entry(frame,width=40,font=('Georgia 30'))
-                e5.pack()
-
-                output=Label(frame,text="")
-                output.pack()
+                e5=Entry(root,width=40,borderwidth = 8,font=('Tektur 25'))
+                e5.pack(padx = 20, pady = 10)
 
 
+                # Set a minsize for the window, and place it in the middle
+                root.update()
+                root.minsize(root.winfo_width(), root.winfo_height())
+                x_cordinate = int((root.winfo_screenwidth() / 2) - (root.winfo_width() / 2))
+                y_cordinate = int((root.winfo_screenheight() / 2) - (root.winfo_height() / 2))
+                root.geometry("+{}+{}".format(x_cordinate, y_cordinate-20))
 
                 def cloud_cover():
                     cloudcover=e2.get()+" "+e3.get()+" "+e4.get()+" "+e5.get()
-                    print(cloudcover)
                     return cloudcover
+
                 def onclic():
                     time.sleep(0)
                     data2=data.decode('utf-8')
-                    print(data2)
                     runway_number=data2[8:10]
                     GST_Time=data2[20:28]
                     Date=data2[29:31]
                     WS_10_min=data2[61:64]
                     WD_10_min=data2[86:88]
-                    print(WD_10_min)
                     Hum_10_min=data2[107:109]
                     Temp_10_min=data2[130:132]
                     DP_10_min=data2[154:156]
                     Pres_10_min=data2[185:190]
                     QNH_10_min=data2[215:219]
                     QFE_10_min=data2[237:240]
-
-                    print(GST_Time)
-                    print(Date)
-                    #declaring all the components of the final METAR Report
                     METAR=""
                     report=" "
                     typ = "METAR"
-                    #-----------------------------------------------------------
-                    #Taking the timestamp for which the user wants the METAR
-                    #Finding the row in the given data which has to be accessed
-                    #row=0
-                    #for i in range(25):
-                    #    if time==df.iloc[i,3]:
-                    #        row=i
-                    #-----------------------------------------------------------
 
-                    #Taking the four terms of cloud cover
-                    #cell_value = df.iloc[row, 28] # cell_value = df.iloc[row_index, column_index]
-                    #pressure=('Q -',cell_value)
-                    #-----------------------------------------------------------
-
-                    #status term which comprises of date,and time in zulu
                     time1=""
                     date=""
                     def status():
@@ -216,42 +164,20 @@ def backend():
                     report=str(status())+'Z'
                     FINAL_METAR=str(typ)+" "+str(report)+" "+str(cloud_cover())+" "+str(finaltempdew)+" "+"Q"+str(QNH_10_min)+" "+"NOSIG="
 
-
-                    gmt = pytz.timezone('GMT')
-                    now = datetime.now(gmt)
-
-                    current_time = now.strftime("%H:%M:%S")
-
-                    ws.append([current_time,FINAL_METAR])
-                    wb.save("IMD_Data.xlsx")
-
-
                     def execute():
                         root.destroy()
                         time.sleep(0)
-                    root.after(2000,execute)
+                    root.after(4000,execute)
 
-
-            #root=Tk()
-            #myLabel=Label(root,text=FINAL_METAR)
-            #myLabel.pack()
-                new_button=Button(root,text="Generate Metar",command=onclic,font=('Georgia 20'),bg="orange",fg="blue")
+                new_button=Button(root,text="Generate Metar",command=onclic,font=('Tektur 15'))
                 new_button.pack()
 
-                #code to exit the screen
-                '''new_button2=Button(root,text="Next",command=root.destroy())
-                new_button2.pack()'''
-
-                #def perform():
-                   # myLabel=Label(root,text="Local")
-                   # myLabel.pack()
-
-                #output.after(10000,perform)
-
-                root.after(5000, end_window)                                                                 #to hold the window for 5 minutes if no action
+                root.after(10000, end_window)
 
                 root.mainloop()
 
-        time.sleep(10) ##This timer can control the time after which the window will pop again
+        time.sleep(20)
+
         frontend()
+
 backend()
